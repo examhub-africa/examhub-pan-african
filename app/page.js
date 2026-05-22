42604e92b4af61070320b2bc78d67347a9e75a93
examhub-africa <jeankellyzlanguage@gmail.com>
2026-05-22 17:34:46 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', paddingTop: '100px', fontFamily: 'sans-serif'}}>
+      <h1>ExamHub Africa is Live</h1>
+      <p>No more 404</p>
+    </div>
+  );
+}
