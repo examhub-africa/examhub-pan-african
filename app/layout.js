bf1c6fb9eb4de27e2730a5482bf45a32ae1b45f3
examhub-africa <jeankellyzlanguage@gmail.com>
2026-05-22 19:46:21 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,11 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#001B55" />
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
