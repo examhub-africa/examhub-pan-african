b7cdbb91930d1218f1942664044f91eff909f87b
examhub-africa <jeankellyzlanguage@gmail.com>
2026-05-22 15:47:23 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-import { AFRICAN_COUNTRIES } from './data/countries.js';
+import { AFRICAN_COUNTRIES } from '../../data/countries.js';
 
 export default function Home() {
   return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">
         Pan-African Exam Hub
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {AFRICAN_COUNTRIES.map((country) => (
-          <a
+          <a 
             key={country.code}
             href={`/${country.code}`}
             className="p-4 border rounded-lg text-center hover:bg-gray-100"
